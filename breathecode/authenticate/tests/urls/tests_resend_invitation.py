3e4ab2bda39557c09eefa7ea0f075d67d4949e4d
jefer94 <jdefreitaspinto@gmail.com>
2021-02-17 14:28:41 -0500

    change method

--- breathecode/authenticate/tests/urls/tests_resend_invitation.py
+++ breathecode/authenticate/tests/urls/tests_resend_invitation.py
@@ -1,5 +1,5 @@
 """
-Test cases for 
+Test cases for
 """
 import re
 from django.urls.base import reverse_lazy
@@ -15,8 +15,8 @@ class AuthenticateTestSuite(AuthTestCase, HeadersMixin):
         self.headers(academy=4)
 
         url = reverse_lazy('authenticate:academy_resent_invite', kwargs={"member_id":2})
-        
-        response = self.client.get(url)
+
+        response = self.client.put(url, {})
         json = response.json()
         expected = {'detail': 'Authentication credentials were not provided.', 'status_code': 401}
 
@@ -29,22 +29,22 @@ class AuthenticateTestSuite(AuthTestCase, HeadersMixin):
         self.generate_models(authenticate=True)
 
         url = reverse_lazy('authenticate:academy_resent_invite', kwargs={"member_id":2})
-        
-        response = self.client.get(url)
+
+        response = self.client.put(url, {})
         json = response.json()
         expected = {'detail': "You (user: 2) don't have this capability: admissions_developer for "
                     'academy 4','status_code': 403}
         self.assertEqual(json, expected)
         self.assertEqual(response.status_code, 403)
-        
+
     def test_resend_invite_with_auth(self):
         """Test """
         self.headers(academy=4)
         self.generate_models(authenticate=True)
 
         url = reverse_lazy('authenticate:academy_resent_invite', kwargs={"member_id":2})
-        
-        response = self.client.get(url)
+
+        response = self.client.put(url, {})
         json = response.json()
         expected = {'detail': "You (user: 2) don't have this capability: admissions_developer for "
                     'academy 4','status_code': 403}
